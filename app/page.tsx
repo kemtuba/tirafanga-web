18a3465f4375c5e30c8514407f39db15e3ca130a
kemtuba <me@kem.design>
2025-06-28 19:24:20 +0100

    removed unused elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 "use client"
 
 // Imports from your existing page
-import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/Button';
 import { ContactForm } from '@/components/ContactForm';
